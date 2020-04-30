7ba59be292b622d4003cc04da9217c0b9e498abf
Johannes E. M. Mosig <j.mosig@rasa.com>
2020-04-30 14:32:57 +0200

    Fix tutorials and NLU start server script

--- parlai/mturk/tasks/woz/backend/bash/start_rasa_nlu_servers.sh
+++ parlai/mturk/tasks/woz/backend/bash/start_rasa_nlu_servers.sh
@@ -10,5 +10,16 @@ $RESOURCES_DIR/ride_status/start_nlu_server.sh $RESOURCES_DIR/ride_status
 $RESOURCES_DIR/hotel_search/start_nlu_server.sh $RESOURCES_DIR/hotel_search
 $RESOURCES_DIR/hotel_reserve/start_nlu_server.sh $RESOURCES_DIR/hotel_reserve
 $RESOURCES_DIR/plane_reserve/start_nlu_server.sh $RESOURCES_DIR/plane_reserve
+$RESOURCES_DIR/apartment_search/start_nlu_server.sh $RESOURCES_DIR/apartment_search
+$RESOURCES_DIR/book_apartment_viewing/start_nlu_server.sh $RESOURCES_DIR/book_apartment_viewing
+$RESOURCES_DIR/book_doctor_appointment/start_nlu_server.sh $RESOURCES_DIR/book_doctor_appointment
+$RESOURCES_DIR/followup_doctor_appointment/start_nlu_server.sh $RESOURCES_DIR/followup_doctor_appointment
+$RESOURCES_DIR/party_plan/start_nlu_server.sh $RESOURCES_DIR/party_plan
+$RESOURCES_DIR/party_rsvp/start_nlu_server.sh $RESOURCES_DIR/party_rsvp
+$RESOURCES_DIR/plane_search/start_nlu_server.sh $RESOURCES_DIR/plane_search
+$RESOURCES_DIR/restaurant_reserve/start_nlu_server.sh $RESOURCES_DIR/restaurant_reserve
+$RESOURCES_DIR/restaurant_search/start_nlu_server.sh $RESOURCES_DIR/restaurant_search
+$RESOURCES_DIR/spaceship_access_codes/start_nlu_server.sh $RESOURCES_DIR/spaceship_access_codes
+$RESOURCES_DIR/spaceship_life_support/start_nlu_server.sh $RESOURCES_DIR/spaceship_life_support
 
 echo "Rasa NLU servers started!"
