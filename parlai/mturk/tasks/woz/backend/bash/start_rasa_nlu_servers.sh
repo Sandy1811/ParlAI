579e2a68d4c136c7041972d59e6492c398e66315
thomas <t.kober@rasa.com>
2020-04-30 09:18:07 +0200

    start all servers was missing some servers

--- parlai/mturk/tasks/woz/backend/bash/start_rasa_nlu_servers.sh
+++ parlai/mturk/tasks/woz/backend/bash/start_rasa_nlu_servers.sh
@@ -10,5 +10,11 @@ $RESOURCES_DIR/ride_status/start_nlu_server.sh $RESOURCES_DIR/ride_status
 $RESOURCES_DIR/hotel_search/start_nlu_server.sh $RESOURCES_DIR/hotel_search
 $RESOURCES_DIR/hotel_reserve/start_nlu_server.sh $RESOURCES_DIR/hotel_reserve
 $RESOURCES_DIR/plane_reserve/start_nlu_server.sh $RESOURCES_DIR/plane_reserve
+$RESOURCES_DIR/apartment_search/start_nlu_server.sh $RESOURCES_DIR/apartment_search
+$RESOURCES_DIR/book_apartment_viewing/start_nlu_server.sh $RESOURCES_DIR/book_apartment_viewing
+$RESOURCES_DIR/book_doctor_appointment/start_nlu_server.sh $RESOURCES_DIR/book_doctor_appointment
+$RESOURCES_DIR/followup_doctor_appointment/start_nlu_server.sh $RESOURCES_DIR/followup_doctor_appointment
+$RESOURCES_DIR/spaceship_access_codes/start_nlu_server.sh $RESOURCES_DIR/spaceship_access_codes
+$RESOURCES_DIR/spaceship_life_support/start_nlu_server.sh $RESOURCES_DIR/spaceship_life_support
 
 echo "Rasa NLU servers started!"
