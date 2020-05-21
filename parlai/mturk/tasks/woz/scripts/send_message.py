88c99d0601a40813c4b45ca4343313ff934b08fe
Johannes E. M. Mosig <j.mosig@rasa.com>
2020-05-21 11:55:42 +0200

    Ask about overtime-disconnect insights

--- parlai/mturk/tasks/woz/scripts/send_message.py
+++ parlai/mturk/tasks/woz/scripts/send_message.py
@@ -20,25 +20,25 @@ def main():
     # )
 
     # workers = ["A1F1OZ54G177D8", "AWPB724OHY99T", "A2WC2NO555XU3J", "A1ZE52NWZPN85P", "A3T01NM4993LZE", "A3TUJHF9LW3M8N", "A2TZAXWOB3JMNV", "A2ER0EVZ7E1Z8G", "A3IDG9C18BCATK", "A1ZE52NWZPN85P", "A1F1OZ54G177D8", "A2TZAXWOB3JMNV", "A2VU8AC7MY3721", "A6AB1O0UYB7PI", "A2MGH3MBXMKD96", "A2TZAXWOB3JMNV", "A1171IQSWQS0K8", "A1ZE52NWZPN85P", "AJQGWGESKQT4Y", "A2MD4K1YFYBLZN", "A2Y5I2HJTRFR8H", "A2TZAXWOB3JMNV", "A1ZE52NWZPN85P", "A3TWYQENN82FJI", "A2HC9549CZAKNN", "AWPB724OHY99T", "ATJVY9O4CY5EX", "A2TZAXWOB3JMNV", "A1171IQSWQS0K8", "A1ZE52NWZPN85P", "A12DY4HHG6MYF0", "A2TZAXWOB3JMNV", "A1BZ1VD8V8VJML", "A1ZE52NWZPN85P", "A3HBMKX19GBOGR", "A1ZE52NWZPN85P", "A6AB1O0UYB7PI", "A3TUJHF9LW3M8N", "A1F1OZ54G177D8", "A6AB1O0UYB7PI", "A37XJVQF62ZYC", "A1K5ILJMG439M5", "A1ZE52NWZPN85P", "AOEO9ZV81R0I4", "A2GJK2MDTHNQ6Q", "A6AB1O0UYB7PI", "A01913642LJT7L89AFM2F", "A3I9XLIHPPWPN1", "AWPB724OHY99T", "A23EWFNNOUS10B", "A3NHJPE6326P9V", "A1ZE52NWZPN85P", "A37XJVQF62ZYC", "A6AB1O0UYB7PI", "A6AB1O0UYB7PI", "AJQGWGESKQT4Y", "A3TUJHF9LW3M8N", "A22TLF121MRXT1", "A2Y5I2HJTRFR8H", "A1ZE52NWZPN85P", "A2JBNDG0U9IA6I", "A3TUJHF9LW3M8N", "A3HBMKX19GBOGR", "A6AB1O0UYB7PI", "A2GJK2MDTHNQ6Q", "A3TUJHF9LW3M8N", "A2HC9549CZAKNN", "AOEO9ZV81R0I4", "A2KW17G25L25R8", "A6AB1O0UYB7PI", "A2TZAXWOB3JMNV", "A2LAE3OM5OQ0WF", "A1UTBDKOQ2MKUD", "A1CY7IOJ9YH136", "A2JBNDG0U9IA6I", "AMELYCC59JKB0", "A2EOOF9D135HQ1", "A2MGH3MBXMKD96", "A6AB1O0UYB7PI", "A1ZE52NWZPN85P", "A1UTBDKOQ2MKUD", "A1ZE52NWZPN85P", "A1UTBDKOQ2MKUD", "A1ZE52NWZPN85P", "A1ZE52NWZPN85P", "A6AB1O0UYB7PI", "A2ER0EVZ7E1Z8G", "AWPB724OHY99T", "A2L73JKDFWI0KJ", "A1ZE52NWZPN85P", "AWPB724OHY99T", "A37XJVQF62ZYC", "ALDQRWYZ8KQ9A", "A6AB1O0UYB7PI", "A2LAE3OM5OQ0WF", "A6AB1O0UYB7PI", "A1UTBDKOQ2MKUD", "A2TZAXWOB3JMNV", "A2TZAXWOB3JMNV", "AWPB724OHY99T", "A1BZ1VD8V8VJML", "A163J5TEJBO43B", "A2EOOF9D135HQ1", "AWPB724OHY99T", "ANBWJZYU2A68T", "A12DY4HHG6MYF0", "A3PYB8Z6FFWSOV", "A1BZ1VD8V8VJML", "A2TZAXWOB3JMNV", "A1ZE52NWZPN85P", "AWPB724OHY99T", "A22TLF121MRXT1", "A39MOCMDRIOSJM", "AOEO9ZV81R0I4", "A3IDG9C18BCATK", "AL8KOIH1DJ7D9", "A1CY7IOJ9YH136", "A6AB1O0UYB7PI", "A2MGH3MBXMKD96", "A1ZE52NWZPN85P", "A3CDG9TL8U99CO", "A1ZE52NWZPN85P", "A6AB1O0UYB7PI", "AWPB724OHY99T", "A3TWYQENN82FJI", "A39MOCMDRIOSJM", "A39MOCMDRIOSJM", "AJQGWGESKQT4Y", "A1UTBDKOQ2MKUD", "A1ZE52NWZPN85P", "A6AB1O0UYB7PI", "AWPB724OHY99T", "A6AB1O0UYB7PI", "A2GJK2MDTHNQ6Q", "A3NHJPE6326P9V", "A6AB1O0UYB7PI", "A1ZE52NWZPN85P", "A6AB1O0UYB7PI", "A2MGH3MBXMKD96", "AL8KOIH1DJ7D9", "A6AB1O0UYB7PI", "A1F1OZ54G177D8", "A33B85TN97HQ33", "A1ZE52NWZPN85P", "A1BZ1VD8V8VJML", "A33B85TN97HQ33", "A2JBNDG0U9IA6I", "A6AB1O0UYB7PI", "A3AIX82WZQZB5A", "AWPB724OHY99T", "A3CDG9TL8U99CO", "ACRTRQ23XH20E", "ATJVY9O4CY5EX", "A2TZAXWOB3JMNV", "A2EOOF9D135HQ1", "A2TZAXWOB3JMNV", "A3NHJPE6326P9V", "A2GJK2MDTHNQ6Q", "ANBWJZYU2A68T", "A2R0YYUAWNT7UD", "A2HC9549CZAKNN", "A2TZAXWOB3JMNV", "A2KW17G25L25R8", "A1ZE52NWZPN85P", "A2EOOF9D135HQ1", "AWPB724OHY99T", "A2KW17G25L25R8", "A6AB1O0UYB7PI"];
-    workers = []
+    workers = ["A272X64FOZFYLB", "A2ER0EVZ7E1Z8G", "A1HAOXJVRYT43K", "A6AB1O0UYB7PI"]
+    # workers = []
 
     while workers:
         worker_batch = workers[:80]
         workers = workers[80:]
         print(f"Sending message to {worker_batch}")
         client.notify_workers(
-            Subject="AI Dialogues - Stage II [v3]",
+            Subject="AI Dialogues - Stage IV Feedback",
             MessageText="Hello! "
                         ""
-                        "As you may have noticed, the AI Dialogues - Stage II [v3] tasks contained a bug that showed you a "
-                        "message saying you can accept the task even though your partner has disconnected - and then "
-                        "rejects your work. We are very sorry for that and now reverted all rejections whenever possible. "
-                        "So your assignment's status should now be switched to 'accepted'. "
+                        "Thank you for participating in the first Stage IV batch of our AI Dialogues. "
+                        "We are still working out some details, but are already quite happy with the dialogues that we've seen. "
+                        "However, we also noticed that nobody has done the long instruction sets (where we pay overtime bonus). "
+                        "I am writing to you to get some feedback on why that might be. "
                         ""
-                        "I also removed all the automatic rejection code, so this should not happen again. Thus, if your "
-                        "partner disconnects early, just return the HIT. If the dialogue was pretty much finished and your "
-                        "partner disconnects, you can submit and we'll read the dialogue and decide if we accept/reject "
-                        "(this might take a while)."
+                        "Is the risk of partner-disconnect to high? Or are long tasks generally not interesting? What can we do to improve? "
+                        ""
+                        "I am looking forward to hear your thoughts on this."
                         ""
                         "\n\nBest wishes\nJohannes",
             WorkerIds=worker_batch
